7181a93798ec1b971b232ead13c1b3ec358ee4a9
Iqbal Ali <iqbalali2606@gmail.com>
2025-01-07 21:47:02 +0700

    style: dashboard

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -6,118 +6,135 @@ import { useState } from "react";
 
 export default function Dashboard({ auth }) {
     const [showDatePicker, setShowDatePicker] = useState(false);
-    const [filter, setfilter] = useState("Pick Date");
+  const [currentTime, setCurrentTime] = useState(new Date());
 
-    const handleFilterClick = (filterType) => {
-        if (filterType === "Pick Date") {
-            setShowDatePicker(true);
-        } else {
-            setShowDatePicker(false);
-            setfilter(filterType);
-            console.log(`Filter applied: ${filterType}`);
-        }
-    };
+  const [filter, setfilter] = useState("Pick Date");
 
-    return (
-        <AuthenticatedLayout
-            user={auth.user}
-            header={
-                <h2 className="font-semibold text-xl text-gray-800 leading-tight">
-                    Dashboard
-                </h2>
-            }
-        >
-            <Head title="Dashboard" />
+  const handleFilterClick = (filterType) => {
+      if (filterType === "Pick Date") {
+          setShowDatePicker(true);
+      } else {
+          setShowDatePicker(false);
+          setfilter(filterType);
+          console.log(`Filter applied: ${filterType}`);
+      }
+  };
 
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="flex space-x-3 justify-end">
-                        {/* Date Picker Modal */}
-                        {showDatePicker && <Datepicker />}
-                        <div className="flex justify-end mb-4 ">
-                            <Dropdown label={`Filter: ${filter}`}>
-                                <Dropdown.Item
-                                    onClick={() =>
-                                        handleFilterClick("Pick Date")
-                                    }
-                                >
-                                    Pick Date
-                                </Dropdown.Item>
-                                <Dropdown.Item
-                                    onClick={() => handleFilterClick("Monthly")}
-                                >
-                                    Monthly
-                                </Dropdown.Item>
-                                <Dropdown.Item
-                                    onClick={() => handleFilterClick("Week")}
-                                >
-                                    Week
-                                </Dropdown.Item>
-                                <Dropdown.Item
-                                    onClick={() => handleFilterClick("Day")}
-                                >
-                                    Day
-                                </Dropdown.Item>
-                            </Dropdown>
-                        </div>
-                    </div>
+  function getGreetingBasedOnTime() {
+      const currentHour = currentTime.getHours();
 
-                    <div className="grid grid-cols-4 gap-x-5">
-                        <article className="rounded-lg border border-gray-200 bg-white p-6">
-                            <div className="flex items-center justify-between">
-                                <div>
-                                    <p className="text-sm text-gray-500 capitalize">
-                                        active users
-                                    </p>
+      if (currentHour >= 5 && currentHour < 11) {
+          return "morning";
+      } else if (currentHour >= 11 && currentHour < 15) {
+          return "afternoon";
+      } else if (currentHour >= 15 && currentHour < 21) {
+          return "evening";
+      } else {
+          return "night";
+      }
+  }
 
-                                    <p className="text-lg font-medium text-gray-900">
-                                        847
-                                    </p>
-                                </div>
-                                <span className="rounded-full bg-green-100 p-3 text-green-600">
-                                    <CircleUserRound />
-                                </span>
-                            </div>
-                        </article>
+  return (
+      <AuthenticatedLayout
+          user={auth.user}
+          header={
+              <h2 className="font-semibold text-xl text-gray-800 leading-tight">
+                  Dashboard
+              </h2>
+          }
+      >
+          <Head title="Dashboard" />
 
-                        <article className="rounded-lg border border-gray-200 bg-white p-6">
-                            <div className="flex items-center justify-between">
-                                <div>
-                                    <p className="text-sm text-gray-500 capitalize">
-                                        Total Chirps
-                                    </p>
+          <div className="py-12">
+              <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                  <h2 className="font-medium text-lg">
+                      Good {getGreetingBasedOnTime()}, {auth.user.name}
+                  </h2>
+                  <div className="flex space-x-3 justify-end">
+                      {/* Date Picker Modal */}
+                      {showDatePicker && <Datepicker />}
+                      <div className="flex justify-end mb-4 ">
+                          <Dropdown label={`Filter: ${filter}`}>
+                              <Dropdown.Item
+                                  onClick={() => handleFilterClick("Pick Date")}
+                              >
+                                  Pick Date
+                              </Dropdown.Item>
+                              <Dropdown.Item
+                                  onClick={() => handleFilterClick("Monthly")}
+                              >
+                                  Monthly
+                              </Dropdown.Item>
+                              <Dropdown.Item
+                                  onClick={() => handleFilterClick("Week")}
+                              >
+                                  Week
+                              </Dropdown.Item>
+                              <Dropdown.Item
+                                  onClick={() => handleFilterClick("Day")}
+                              >
+                                  Day
+                              </Dropdown.Item>
+                          </Dropdown>
+                      </div>
+                  </div>
 
-                                    <p className="text-lg font-medium text-gray-900">
-                                        847
-                                    </p>
-                                </div>
+                  <div className="grid grid-cols-4 gap-x-5">
+                      <article className="rounded-lg border border-gray-200 bg-white p-6">
+                          <div className="flex items-center justify-between">
+                              <div>
+                                  <p className="text-sm text-gray-500 capitalize">
+                                      active users
+                                  </p>
 
-                                <span className="rounded-full bg-gray-100 p-3 text-gray-600">
-                                    <Bird />
-                                </span>
-                            </div>
-                        </article>
+                                  <p className="text-lg font-medium text-gray-900">
+                                      847
+                                  </p>
+                              </div>
+                              <span className="rounded-full bg-green-100 p-3 text-green-600">
+                                  <CircleUserRound />
+                              </span>
+                          </div>
+                      </article>
 
-                        <article className="rounded-lg border border-gray-200 bg-white p-6">
-                            <div className="flex items-center justify-between">
-                                <div>
-                                    <p className="text-sm text-gray-500 capitalize">
-                                        violation report
-                                    </p>
+                      <article className="rounded-lg border border-gray-200 bg-white p-6">
+                          <div className="flex items-center justify-between">
+                              <div>
+                                  <p className="text-sm text-gray-500 capitalize">
+                                      Total Chirps
+                                  </p>
 
-                                    <p className="text-lg font-medium text-gray-900">
-                                        847
-                                    </p>
-                                </div>
+                                  <p className="text-lg font-medium text-gray-900">
+                                      847
+                                  </p>
+                              </div>
 
-                                <span className="rounded-full bg-red-100 p-3 text-red-600">
-                                    <MessageCircleWarning />
-                                </span>
-                            </div>
-                        </article>
-                    </div>
-                </div>
-            </div>
-        </AuthenticatedLayout>
-    );
+                              <span className="rounded-full bg-gray-100 p-3 text-gray-600">
+                                  <Bird />
+                              </span>
+                          </div>
+                      </article>
+
+                      <article className="rounded-lg border border-gray-200 bg-white p-6">
+                          <div className="flex items-center justify-between">
+                              <div>
+                                  <p className="text-sm text-gray-500 capitalize">
+                                      violation report
+                                  </p>
+
+                                  <p className="text-lg font-medium text-gray-900">
+                                      847
+                                  </p>
+                              </div>
+
+                              <span className="rounded-full bg-red-100 p-3 text-red-600">
+                                  <MessageCircleWarning />
+                              </span>
+                          </div>
+                      </article>
+                  </div>
+              </div>
+          </div>
+      </AuthenticatedLayout>
+  );
 }
